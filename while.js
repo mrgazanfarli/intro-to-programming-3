3b3745facab7ed1e7bbbaf6903043ac615546426
mrgazanfarli <mr.gazanfarli@gmail.com>
2020-11-21 13:29:18 +0400

    added prime number checker

--- while.js
+++ while.js
@@ -5,17 +5,54 @@
 
 console.clear();
 
-function getDigitCountOf(number) {
-    let count = 0;
+// function getDigitCountOf(number) {
+//     let count = 0;
 
-    while(number !== 0) {
-        number = (number - number % 10) / 10;
-        count++;
+//     while(number !== 0) {
+//         number = (number - number % 10) / 10;
+//         count++;
+//     }
+
+//     return count;
+// }
+
+// const digitsCount = getDigitCountOf(3);
+
+// console.log(digitsCount);
+
+// Get the number of prime numbers
+
+// 1---20 -> 2, 3, 5, 7, 11, 13, 17, 19
+// 29
+// 2 - 28
+// 2 - root of 29
+
+function isPrime(number) {
+    const root = Math.sqrt(number);
+
+    for (let i = 2; i < root; i++) {
+        if (number % i === 0) {
+            return false;
+        }
     }
-    
-    return count;
+
+    return true;
 }
 
-const digitsCount = getDigitCountOf(3);
+function getNumberOfPrimeNumbers(lowerBound, higherBound = 100) {
+    let result = [];
 
-console.log(digitsCount);
+    if (lowerBound <= 1) {
+        lowerBound = 2;
+    }
+
+    for (let i = lowerBound; i <= higherBound; i++) {
+        if (isPrime(i)) {
+            result.push(i);
+        }
+    }
+
+    return result;
+}
+
+console.log(getNumberOfPrimeNumbers(1, 20));
