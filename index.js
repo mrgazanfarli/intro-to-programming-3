2782769a65e3e250f2dbb063cddfd99e67606709
mrgazanfarli <mr.gazanfarli@gmail.com>
2020-11-21 10:44:35 +0400

    added reverse string function

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('test');
+console.clear();
+
+// Reversing a string
+const str1 = 'hello world';
+
+function reverseString(str) {
+    let result = '';
+    
+    for (let i = str.length - 1; i >= 0; i-- ) {
+        result = result + str[i];
+    }
+    
+    return result;
+}
+
+const reversedStr1 = reverseString(str1);
+
+console.log(reversedStr1);
