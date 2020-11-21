57f581448d09126b1a1d7d56f97fbe8ee02c91e5
mrgazanfarli <mr.gazanfarli@gmail.com>
2020-11-21 11:37:44 +0400

    added a function to reverse an array

---
+++ index-2.js
@@ -0,0 +1,16 @@
+console.clear();
+
+function reverseArray(arr) {
+    const result = [];
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        result.push(arr[i]);
+    }
+
+    return result;
+}
+
+const numbers = [1, 2, 3, 4, 5, 6];
+const reversedNumbers = reverseArray(numbers);
+
+console.log(reversedNumbers);
