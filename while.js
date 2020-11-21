7891c829cef114572f1478c7054f755dfea8911c
mrgazanfarli <mr.gazanfarli@gmail.com>
2020-11-21 12:50:20 +0400

    added number of digits function

---
+++ while.js
@@ -0,0 +1,21 @@
+// Finding the number of digits in a number
+
+// 123 -> 3
+// 1000 -> 4
+
+console.clear();
+
+function getDigitCountOf(number) {
+    let count = 0;
+
+    while(number !== 0) {
+        number = (number - number % 10) / 10;
+        count++;
+    }
+
+    return count;
+}
+
+const digitsCount = getDigitCountOf(1243);
+
+console.log(digitsCount);
